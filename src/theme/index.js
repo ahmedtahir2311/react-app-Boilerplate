df126d9b52bcd5c1973718835a7dc4fa534b62f1
ahmedtahir2311 <ahmed@codersdeck.co>
2022-09-13 10:20:48 -0700

    ThemeProvider setUp

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,11 +1,62 @@
-import { createTheme } from "@mui/material";
+import {
+  createTheme,
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeProvider,
+} from "@mui/material";
+import ComponentsOverrides from "./components";
+import mainPalette from "./palette";
+import typography from "./typography";
 
-const ThemeConfigration = () => {
-  const appytheme = () => {
-    const theme = createTheme();
+const ThemeConfigration = ({ children }) => {
+  const applyTheme = () => {
+    // simple theme cratation
+    let theme = createTheme({
+      palette: mainPalette,
+    });
+
+    return createTheme(theme, {
+      components: ComponentsOverrides(theme),
+      typography: typography(theme),
+    });
+
+    // 1. This is the sceniorio of Using ight and Dark theme
+
+    ////////////////////////////////2.This theme is created on the bases of Two differnt Portals //////////////////////////////
+    //   let theme = createTheme({
+    //     palette:
+    //       isUser(route) || isProductsDetail(route) || allowBuyerTheme    //3.Some Condition to apply different theme Theme
+    //         ? buyerPalette
+    //         : palette,
+    //   });
+    //   if (isUser(route) || isProductsDetail(route) || allowBuyerTheme) {   //4. Some Condition to apply different theme Theme
+
+    //     return createTheme(theme, {
+    //       components: buyerComponentsOverrides(theme),
+    //       typography: buyerTypography(theme),
+    //       shadows,
+    //     });
+    //   } else {
+    //     return createTheme(theme, {                        //5.Some Condition to apply different theme Theme
+    //       components: componentsOverride(theme),
+    //       typography: typography(theme),
+    //       shape,
+    //     });
+    //   }
+    // };
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   };
 
-  return <></>;
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={applyTheme()}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 };
 
 export default ThemeConfigration;
+
+// by Wrapping this component tothe mainthing it will apply theme to the component
