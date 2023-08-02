69ddcc48d6ac5d149de5b09d4fa64af46b9b26ec
ahmed.tahir <ahmed.tahir@sprintx.net>
2023-08-02 16:23:57 +0500

    drag n drop added

--- src/constants/elements.js
+++ src/constants/elements.js
@@ -1,9 +1,10 @@
-import Home from "../pages/home/";
-import SignIn from "../pages/auth/signIn/SignIn";
-import SignUp from "../pages/auth/signUp/SignUp";
-import ErrorComponent from "../pages/ErrorBoundaries/ErrorComponent";
-import ErrorComponentUsingLibrary from "../pages/ErrorBoundaries/ErrorComponentUsingLibrary";
-import Form from "../pages/Form/";
+import Home from "pages/home/";
+import SignIn from "pages/auth/signIn/SignIn";
+import SignUp from "pages/auth/signUp/SignUp";
+import ErrorComponent from "pages/ErrorBoundaries/ErrorComponent";
+import ErrorComponentUsingLibrary from "pages/ErrorBoundaries/ErrorComponentUsingLibrary";
+import Form from "pages/Form/";
+import Draggable from "pages/Draggable";
 
 export {
   Home,
@@ -12,4 +13,5 @@ export {
   ErrorComponent,
   ErrorComponentUsingLibrary,
   Form,
+  Draggable,
 };
---
+++ src/pages/Draggable/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import DraggableComponent from "components/Draggable/Draggable";
+
+const Index = () => {
+  return (
+    <div>
+      See Code for understanding
+      {/* <DraggableComponent /> */}
+      {/* //See Component for table Draggable  */}
+    </div>
+  );
+};
+
+export default Index;
